197fbd6308a54d7086d0ebdc6744035cc4459dd2
RakeshCheekatimala <rakeshcheekatimala@gmail.com>
2019-11-20 13:39:03 +0800

    small css changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with CI CD using Travis CI
+          Learn React
         </a>
+        <div className="TechStack">
+          <p>
+            React App Integrated with Travis for CI/CD to AWS ElasticBeanStalk
+          </p>
+          <ul>
+            <li>React</li>
+            <li>Travis CI</li>
+            <li>AWS Elastic BeanStalk</li>
+          </ul>
+        </div>
       </header>
     </div>
   );
